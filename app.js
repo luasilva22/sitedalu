db0d3d6da12137d77bcd08ab7882ee4929c36cea
luasilva22 <luizasilvamarques33@gmail.com>
2026-05-08 19:55:21 -0300

    primeiro site
    Fix HTML structure and ushusuhdadd content for the site.

---
+++ app.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+</html
+<head>
+  <title>Site da lu</title>meu primeiro site publicado no git
+</head>
+<body>
+  <h1>Oi,eu sou a Lu</h1>h1>
+  <p>Esse é meu primeiro site publicado no github!</p>
+</body>
+</hthl>
